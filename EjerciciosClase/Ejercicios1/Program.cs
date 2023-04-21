27e245e27b84fe27ac210a285ff9c12cdf706247
sagutierrezz <santi.gutix93@gmail.com>
2023-04-21 00:13:42 -0300

    commit 3

--- EjerciciosClase/Ejercicios1/Program.cs
+++ EjerciciosClase/Ejercicios1/Program.cs
@@ -1,13 +1,180 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+//----------------------------------------FOR---------------------------------------------//
+
+// 5- El usuario ingresa 5 pares de valores con el nombre del cliente y el total gastado, indicar el cliente que gastó más.
+
+/*
+int elMayorGasto = 0;
+string elNombreDelFlacoQueMasGasto = "";
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine("Porfa ingreseme el nombre del pibe ");
+    string nombre = Console.ReadLine();
+
+    Console.WriteLine("Porfa ingreseme cuanto gastó ");
+    int gasto = int.Parse(Console.ReadLine());
+
+    if (gasto > elMayorGasto)
+    {
+        // Aca tengo un nuevo gasto mayor, así que tengo que hacer 2 cosas
+        // una es ahora tener este nuevo gasto como mayor
+        // y lo otro es no perder el nombre del flaco, porque me piden que al final
+        // diga quien es este pibe que mas gastó
+        elMayorGasto = gasto;
+        elNombreDelFlacoQueMasGasto = nombre;
+    }
+}
+Console.WriteLine("El pibe que más gastó fue " + elNombreDelFlacoQueMasGasto);
+Console.WriteLine(" Y el total que gastó fue " + elMayorGasto);
+*/
+
+// 4- De una lista de 8 números indicar cual es el mayor
+
+/*
+int[] numeros = new int[] { 5, 2, 10, 8, 1, 9, 4, 7 };
+
+int maximo = numeros[0]; //toma como máximo el primer valor de la lista, para luego en cada iteración compararlo con el elemento que sigue de la lista
+
+for (int i = 1; i < numeros.Length; i++) ??// mientras i sea menor a la cantidad de elementos de la lista se ejecutará, cuando i es mayor a la cantidad de elementos de la lista, finaliza el bucle
+{
+    if (numeros[i] > maximo) 
+    {
+        maximo = numeros[i]; // si el elemento de la lista es mayor que maximo lo guarda en la variable 'maximo'
+    }
+}
+
+Console.WriteLine($"El número máximo de la lista 'numeros' es: {maximo}");
+*/
+
+
+// 3- Para calcular el total de una factura , el usuario ingresará pares de datos Costo y Total ;
+// de unas 5 compras; informar el total adeudado.
+
+/*
+Console.WriteLine("Porfa ingreseme el costo");
+string costo = Console.ReadLine();
+int costoNum = int.Parse(costo);
+int sumita = 0;
+
+for (int i = 0; i < 5; i++)
+{
+
+    Console.WriteLine("Ahora poneme el total");
+    string total = Console.ReadLine();
+    int totalNum = int.Parse(total);
+
+    
+    sumita = sumita + (costoNum * totalNum);
+
+    Console.WriteLine("Porfa ingreseme el costo");
+    costo = Console.ReadLine();
+    costoNum = int.Parse(costo);
+}
+
+Console.WriteLine("Flaco , pusiste: " + sumita);
+*/
+
+
+
+// 2- Se necesita calcular el promedio de 8 números ingresados por el usuario
+
+/*
+int numero = 0;
+int suma = 0;
+double promedio = 0;
+for (int i = 0; i < 8; i++)
+{
+    Console.WriteLine("Ingresa un numero");
+    numero = int.Parse(Console.ReadLine());
+
+    suma += numero;
+
+    
+}
+promedio = suma / 8;
+Console.WriteLine($"El promedio de los numeros ingresados es: {promedio}");
+*/
+
+
+// 1- El usuario necesita ingresar 5 valores enteros, el sistema deberá informar la suma de los 5
+
+/*
+int numero = 0;
+int suma = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Ingresa un numero");
+    numero = int.Parse(Console.ReadLine());
+
+    suma += numero;
+}
+Console.WriteLine($"La suma de los 5 numeros es: {suma}");
+*/
+
+
+
+
 ////////////////////////-------------------------WHILE--------------------------///////////////////////-------------------
 
+
+// 5- Para calcular el consumo total de un cliente, el usuario primero ingresará cuántos productos compró;
+// luego el sistema solicitará que ingrese el total y costo de cada uno de esos productos consumidos;
+// al terminar informará el total adeudado por el cliente. 
+
+/*
+int cantidadProductos;
+double costoProducto, totalProducto, Total = 0;
+
+Console.Write("Ingrese la cantidad de productos: ");
+cantidadProductos = int.Parse(Console.ReadLine());
+
+int i = 1; //Inicializo i en 1 para que el primer write del while comience en 1
+while (i <= cantidadProductos) 
+{
+    Console.Write($"Ingrese el costo del producto {i}: ");
+    costoProducto = double.Parse(Console.ReadLine());
+
+    Console.Write($"Ingrese el total del producto {i}: ");
+    totalProducto = double.Parse(Console.ReadLine());
+
+    Total += (totalProducto - costoProducto);
+    i++;
+}
+
+Console.WriteLine($"El total adeudado por el cliente es: {Total}");
+*/
 
 // 4- Para calcular el total de una factura , el usuario ingresará pares de datos Costo y Total
 //; cuando se ingrese costo 0 finaliza el ingreso de datos; informar el total adeudado.
 
+/*
+Console.WriteLine("Porfa ingreseme el costo");
+string costo = Console.ReadLine();
+int costoNum = int.Parse(costo);
+int sumita = 0;
 
-//3- El usuario ingresará una lista de nombre de persona, cuando se ingresé un nombre vacío, sin datos;
+while (costoNum > 0)
+{
+
+    Console.WriteLine("Ahora poneme el total");
+    string total = Console.ReadLine();
+    int totalNum = int.Parse(total);
+
+    // En estancia yo ya tengo ingresado al menos 1 par de costo y total
+    // así ´que acá sería un buen momento de hacer mi proceso
+    sumita = sumita + (costoNum * totalNum);
+
+    Console.WriteLine("Porfa ingreseme el costo");
+    costo = Console.ReadLine();
+    costoNum = int.Parse(costo);
+}
+
+Console.WriteLine("Flaco , pusiste: " + sumita);
+*/
+
+// 3- El usuario ingresará una lista de nombre de persona, cuando se ingresé un nombre vacío, sin datos;
 //el sistema informará cuántos nombres se han ingresado.
 
 /*
@@ -31,7 +198,7 @@ Console.WriteLine($"Ingresaste un total de {contador} " );
 
 
 
-//2- Siguiendo el ejercicio anterior, calcular el promedio de los valores ingresados
+// 2- Siguiendo el ejercicio anterior, calcular el promedio de los valores ingresados
 
 /*
 Console.WriteLine("Ingresa un número positivo");
@@ -63,7 +230,7 @@ while (numeroEntero > 0)
 */
 
 
-//1 - El usuario ingresará valores positivos, cuando no tenga más valores ingresará 0 (cero)
+// 1 - El usuario ingresará valores positivos, cuando no tenga más valores ingresará 0 (cero)
 //El sistema deberá informar la suma de los ingresados
 
 /*
@@ -90,8 +257,6 @@ while (numeroEntero > 0)
     }
 }
 */
-
-
 
 
 //-----------------------------------------------------------------------------------------------------------------------------//
@@ -182,7 +347,9 @@ Console.WriteLine("Bien hecho");
 */
 
 
-//1-Utilizando el DO. De esta forma utiliza un console.writeline
+//1- Para el ingreso a un sistema se necesita solicitar nombre de usuario y clave,
+//repetir este ingreso hasta que el usuario ingrese la clave correcta: 667788
+
 /*
 Console.WriteLine("Ingresa una clave");
 string? clave;
@@ -203,13 +370,7 @@ Console.WriteLine("Perfecto pibe");
 
 
 
-
-
-
-
-
-
-//----------------------------------------------------------------------------------------------------------------//
+//---------------------Ejercicios de la primer clase-------------------------------------------------------------------//
 //Para el ingreso a un sistema se necesita solicitar nombre de usuario y clave,
 //repetir este ingreso hasta que el usuario ingrese la clave correcta: 667788
 
